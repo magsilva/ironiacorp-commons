9269c57c391ca9b73d370dbcc1c6d2f512ab9b73
magsilva <magsilva@299673a3-5718-494b-b485-2957c0d3488e>
2010-06-03 18:49:36 +0000

    - Initial import. The first data structure implemented is a generic, non-optimized, tree structure.

---
+++ Components/DataStructures/src/com/ironiacorp/datastructure/tree/GenericTreeTraversalOrderEnum.java
@@ -0,0 +1,11 @@
+/*
+ Copyright 2010 Vivin Suresh Paliath
+ Distributed under the BSD License
+*/
+
+package com.ironiacorp.datastructure.tree;
+
+public enum GenericTreeTraversalOrderEnum {
+    PRE_ORDER,
+    POST_ORDER
+}
