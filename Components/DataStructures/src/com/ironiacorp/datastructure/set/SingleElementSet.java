6adf029b2e2e677f0e9be7506f00a8d27798496d
magsilva <magsilva@299673a3-5718-494b-b485-2957c0d3488e>
2011-03-03 00:19:45 +0000

    - Added a new data type (SingleElementSet). It is kidda odd to have   a set with only one element, but, for resource saving reasons, it   looks useful.

---
+++ Components/DataStructures/src/com/ironiacorp/datastructure/set/SingleElementSet.java
@@ -0,0 +1,205 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+Copyright (C) 2011 Marco Aurelio Graciotto Silva <magsilva@ironiacorp.com>
+*/
+
+package com.ironiacorp.datastructure.set;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+public class SingleElementSet<E> implements Set<E>
+{
+	private E element;
+	
+	private class SingleElementSetIterator implements Iterator<E> {
+		private boolean unread = true;
+		
+		@Override
+		public boolean hasNext() {
+			if (element == null) {
+				return false;
+			} else {
+				return unread;
+			}
+		}
+
+		@Override
+		public E next() {
+			if (unread) {
+				unread = false;
+				if (element == null) {
+					throw new NoSuchElementException();
+				} else {
+					return element;
+				}
+			} else {
+				throw new NoSuchElementException();
+			}
+		}
+
+		@Override
+		public void remove() {
+			if (unread || element == null) {
+				throw new IllegalStateException();
+			}
+			element = null;
+		}
+	};
+	
+	@Override
+	public int size() {
+		return element == null ? 0 : 1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return (element == null);
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (o == null) {
+			return false;
+		}
+		return o.equals(element);
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new SingleElementSetIterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] array = (isEmpty() ? new Object[0] : new Object[1]);
+		if (! isEmpty()) {
+			array[0] = element;
+		}
+		return array;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		int size = (isEmpty() ? 0 : 1);
+		if (a.length < size) {
+			a = (T[])java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+		}
+		if (size == 1) {
+			a[0] = (T) element;
+			if (a.length > 1) {
+				a[1] = null;
+			}
+		}
+		return a;
+	}
+
+
+	@Override
+	public boolean add(E e) {
+		if (element == null) {
+			element = e;
+			return true;
+		}
+		if (element == e) {
+			return false;
+		}
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (element == o) {
+			clear();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		if (c.size() == 1) {
+			return c.contains(element);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		Iterator<?> i = c.iterator();
+		while (i.hasNext()) {
+			Object object = i.next();
+			if (object != null) {
+				if (object.equals(element)) {
+					return false;
+				}
+			}
+		}
+		
+		clear();
+		return true;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		Iterator<?> i = c.iterator();
+		while (i.hasNext()) {
+			Object object = i.next();
+			if (object != null) {
+				if (object.equals(element)) {
+					clear();
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		element = null;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		return prime + ((element == null) ? 0 : element.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SingleElementSet other = (SingleElementSet) obj;
+		if (element == null) {
+			if (other.element != null)
+				return false;
+		} else if (!element.equals(other.element))
+			return false;
+		return true;
+	}
+}
---
+++ Components/DataStructures/test/com/ironiacorp/datastructure/set/SingleElementSetTest.java
@@ -0,0 +1,373 @@
+package com.ironiacorp.datastructure.set;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SingleElementSetTest {
+	
+	SingleElementSet<String> set;
+	
+	boolean result;
+
+	@Before
+	public void setUp() throws Exception {
+		set = new SingleElementSet<String>();
+	}
+
+	@Test
+	public void testSize_Empty() {
+		assertEquals(0, set.size());
+	}
+
+	@Test
+	public void testSize_Full() {
+		set.add("a");
+		assertEquals(1, set.size());
+	}
+
+
+	@Test
+	public void testIsEmpty_Empty() {
+		assertTrue(set.isEmpty());
+	}
+
+	@Test
+	public void testIsEmpty_Full() {
+		set.add("a");
+		assertFalse(set.isEmpty());
+	}
+
+	@Test
+	public void testIsEmpty_Full_Empty() {
+		set.add("a");
+		set.clear();
+		assertTrue(set.isEmpty());
+	}
+	
+	@Test
+	public void testContains_Fail() {
+		assertFalse(set.contains("a"));
+	}
+	@Test
+	public void testContains_Null() {
+		assertFalse(set.contains(null));
+	}
+	
+	@Test
+	public void testContains_Success1() {
+		set.add("a");
+		assertTrue(set.contains("a"));
+	}
+	
+	@Test
+	public void testContains_Success2() {
+		set.add("a");
+		set.clear();
+		assertFalse(set.contains("a"));
+	}
+
+	@Test
+	public void testIterator_Empty() {
+		Iterator<String> i = set.iterator();
+		assertFalse(i.hasNext());
+	}
+
+	@Test(expected=NoSuchElementException.class)
+	public void testIterator_Empty2() {
+		Iterator<String> i = set.iterator();
+		i.next();
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public void testIterator_Empty3() {
+		Iterator<String> i = set.iterator();
+		try {
+			i.next();
+		} catch (Exception e) {
+		}
+		i.next();
+	}
+
+	@Test
+	public void testIterator_Remove_Full() {
+		set.add("a");
+		Iterator<String> i = set.iterator();
+		i.next();
+		i.remove();
+		assertTrue(set.isEmpty());
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void testIterator_Remove_Empty() {
+		Iterator<String> i = set.iterator();
+		i.remove();
+	}
+
+	
+	@Test
+	public void testIterator_Full() {
+		set.add("a");
+		Iterator<String> i = set.iterator();
+		int count = 0;
+		assertTrue(i.hasNext());
+		while (i.hasNext()) {
+			count++;
+			String text = i.next();
+			assertEquals("a", text);
+		}
+		assertEquals(1, count);
+	}
+
+	@Test
+	public void testToArray_Empty() {
+		String[] expected = new String[0];
+		Object[] result = set.toArray();
+		assertEquals(0, result.length);
+	}
+	
+	@Test
+	public void testToArray_Full() {
+		String[] expected = new String[1];
+		expected[0] = "a";
+		set.add("a");
+		Object[] result = set.toArray();
+		assertEquals("a", result[0]);
+	}
+
+	@Test
+	public void testToArrayTArray_Empty() {
+		String[] expected = new String[0];
+		String[] result = (String[]) set.toArray(new String[0]);
+		assertArrayEquals(expected, result);
+	}
+
+	@Test
+	public void testToArrayTArray_Full_EmptyResultArray() {
+		String[] expected = new String[1];
+		expected[0] = "a";
+		set.add("a");
+		String[] result = (String[]) set.toArray(new String[0]);
+		assertArrayEquals(expected, result);
+	}
+
+	@Test
+	public void testToArrayTArray_Full_BigResultArray() {
+		String[] expected = new String[1];
+		String[] seed = new String[10];
+		expected[0] = "a";
+		set.add("a");
+		String[] result = (String[]) set.toArray(seed);
+		assertSame(seed, result);
+		assertEquals(expected[0], result[0]);
+		assertEquals(null, result[1]);
+	}
+
+	
+	@Test
+	public void testAdd() {
+		result = set.add("a");
+		assertTrue(result);
+		assertFalse(set.isEmpty());
+		assertTrue(set.contains("a"));
+	}
+
+	@Test
+	public void testAdd_Full() {
+		set.add("a");
+		result = set.add("a");
+		assertFalse(result);
+		assertFalse(set.isEmpty());
+		assertTrue(set.contains("a"));
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void testAdd_Full2_1() {
+		set.add("a");
+		set.add("b");
+	}
+
+	@Test
+	public void testAdd_Full2_2() {
+		try {
+			set.add("a");
+			set.add("b");
+		} catch (UnsupportedOperationException e) {
+			assertTrue(set.contains("a"));
+		}
+	}
+
+	
+	@Test
+	public void testRemove() {
+		set.add("a");
+		result = set.remove("a");
+		assertTrue(result);
+		assertTrue(set.isEmpty());
+		assertFalse(set.contains("a"));
+	}
+
+	@Test
+	public void testRemoveFail() {
+		set.add("a");
+		result = set.remove("b");
+		assertFalse(result);
+		assertFalse(set.isEmpty());
+		assertTrue(set.contains("a"));
+	}
+	
+	@Test
+	public void testContainsAll_Fail() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set2.add("a");
+		set.add("a");
+		assertFalse(set.containsAll(set2));
+	}
+
+	@Test
+	public void testContainsAll_Success() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("a");
+		set.add("a");
+		assertTrue(set.containsAll(set2));
+	}
+
+	@Test(expected=UnsupportedOperationException.class)
+	public void testAddAll() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set2.add("a");
+		set.addAll(set2);
+	}
+
+	@Test
+	public void testRetainAll_Success() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set2.add("a");
+		set.add("b");
+		result = set.retainAll(set2);
+		assertTrue(result);
+		assertTrue(set.isEmpty());
+	}
+
+	@Test
+	public void testRetainAll_Failure() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set2.add("a");
+		set.add("a");
+		result = set.retainAll(set2);
+		assertFalse(result);
+		assertFalse(set.isEmpty());
+		assertTrue(set.contains("a"));
+	}
+	
+	@Test
+	public void testRemoveAll_Success() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set2.add("a");
+		set.add("a");
+		result = set.removeAll(set2);
+		assertTrue(result);
+		assertTrue(set.isEmpty());
+	}
+
+	@Test
+	public void testRemoveAll_Fail() {
+		Set<String> set2 = new HashSet<String>();
+		set2.add("abc");
+		set2.add("def");
+		set.add("a");
+		result = set.removeAll(set2);
+		assertFalse(result);
+		assertFalse(set.isEmpty());
+		assertTrue(set.contains("a"));
+	}
+
+	@Test
+	public void testClear_Empty() {
+		set.clear();
+		assertTrue(set.isEmpty());
+	}
+
+	@Test
+	public void testClear_Full() {
+		set.add("a");
+		set.clear();
+		assertTrue(set.isEmpty());
+	}
+	
+	@Test
+	public void testHashCode_Empty() {
+		assertEquals(31, set.hashCode());
+	}
+	
+	@Test
+	public void testHashCode_Full() {
+		set.add("a");
+		assertTrue(31 != set.hashCode());
+	}
+	
+	@Test
+	public void testEquals_Empty_Empty()
+	{
+		SingleElementSet<String> set2 = new SingleElementSet<String>();
+		assertTrue(set.equals(set2));
+	}
+	
+	@Test
+	public void testEquals_Empty_Full()
+	{
+		SingleElementSet<String> set2 = new SingleElementSet<String>();
+		set2.add("a");
+		assertFalse(set.equals(set2));
+	}
+	
+	@Test
+	public void testEquals_Full_Empty()
+	{
+		SingleElementSet<String> set2 = new SingleElementSet<String>();
+		set.add("a");
+		assertFalse(set.equals(set2));
+	}
+
+	@Test
+	public void testEquals_Empty_Null()
+	{
+		assertFalse(set.equals(null));
+	}
+	
+	@Test
+	public void testEquals_Full_Null()
+	{
+		set.add("a");
+		assertFalse(set.equals(null));
+	}
+
+	@Test
+	public void testEquals_Same()
+	{
+		assertTrue(set.equals(set));
+	}
+
+	@Test
+	public void testEquals_Different()
+	{
+		assertFalse(set.equals("a"));
+	}
+
+}
