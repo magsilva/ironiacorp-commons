c13b2a754a1fff25fdf3a7cdf570363f9f4a9571
magsilva <magsilva@299673a3-5718-494b-b485-2957c0d3488e>
2011-08-01 20:40:08 +0000

    - Add copyright header to test sets. - Disable broken test case at 'GenericsTest'.

--- Components/Introspector/test/com/ironiacorp/introspector/GenericsTest.java
+++ Components/Introspector/test/com/ironiacorp/introspector/GenericsTest.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright (C) 2007 Marco Aurélio Graciotto Silva <magsilva@ironiacorp.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package com.ironiacorp.introspector;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 
 public class GenericsTest
 {
@@ -34,6 +51,7 @@ public class GenericsTest
 		assertEquals(String.class, dummy.getGenericParameterType());
 	}
 	
+	@Ignore
 	@Test
 	public void testGenerics2()
 	{
