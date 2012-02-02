68fb94066547649e9b3b10123551574d0dfd5d80
magsilva <magsilva@299673a3-5718-494b-b485-2957c0d3488e>
2012-02-02 16:59:45 +0000

    - Import alien code for reading/writing bits from/to a stream.

---
+++ commons-io/src/com/ironiacorp/io/BitInputStream.java
@@ -0,0 +1,134 @@
+package com.ironiacorp.io;
+
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
+/**
+ * Version 1.0
+ * Released under the GNU GPL
+ * @author steev
+ *
+ */
+public class BitInputStream {
+
+	InputStream thisStream;
+	short currentByte;
+	int bitReady;	// which bit do we retrieve next. 8 for MSB, 1 for LSB, 0 for load next
+	
+	private BitInputStream() {}
+	
+	public BitInputStream(InputStream is) {
+		thisStream = is;
+		bitReady = 0;
+	}
+	
+	public boolean getBoolean() throws java.io.IOException {
+		return getBits(1) != 0 ? true : false;
+	}
+	
+	public byte getByte() throws java.io.IOException {
+		return (byte)getBits(8);
+	}
+	
+	public short getShort() throws java.io.IOException {
+		return (short)getBits(16);
+	}
+	
+	public int getInt() throws java.io.IOException {
+		return (int)getBits(32);
+	}
+	
+	public long getLong() throws java.io.IOException {
+		return (long)getBits(64);
+	}
+	
+	public void getArray(short[] b) throws java.io.IOException {
+		getArray(b, b.length * 8);
+	}
+	
+	public void getArray(short[] b, int numBits) throws java.io.IOException {
+		int idx = 0;
+		if (numBits > b.length*8) {
+			numBits = b.length*8;
+		}
+		
+		while(numBits > 0) {
+			int count = numBits > 8 ? 8 : numBits;
+			b[idx] = (short)getBits(count);
+			numBits -= count;
+			++idx;
+		}
+	}
+	
+	public void getArray(byte[] b) throws java.io.IOException {
+		getArray(b, b.length * 8);
+	}
+	
+	public void getArray(byte[] b, int numBits) throws java.io.IOException {
+		int idx = 0;
+		if (numBits > b.length*8) {
+			numBits = b.length*8;
+		}
+		
+		while(numBits > 0) {
+			int count = numBits > 8 ? 8 : numBits;
+			b[idx] = (byte)getBits(count);
+			numBits -= count;
+			++idx;
+		}
+	}
+	
+	public long getBits(int numBits)  throws java.io.IOException {
+		long result = 0;	// work with long to get as many digits of accuracy as possible
+		while(numBits > 0) {
+			// No more data left, so refill buffer
+			if (bitReady == 0) {
+				currentByte = getNextByte();
+				bitReady = 8;
+			}
+			// 87654321
+			// 76543210
+			int shift = bitReady-numBits;
+			if (shift >= 0) {
+				// There are enough bits left in the current byte to take them all
+				result <<= numBits; 
+				result |= (currentByte >> shift) & ((1<<numBits)-1);
+				bitReady -= numBits;
+				return result;
+			} else {
+				// Read as many bits as we have left in the current byte
+				// We then continue the loop.
+				result <<= bitReady; 
+				result |= (currentByte & ((1<<bitReady)-1));
+				numBits -= bitReady;
+				bitReady = 0;
+			}
+		}
+
+		return result;
+	}
+		
+	short getNextByte() throws java.io.IOException {
+		// This casting ensures we can get unsigned data out
+		return (short)(0xff & (int)thisStream.read());
+	}
+	
+	//
+	// Utility functions
+	//
+	/**
+	 * This loads in all the bytes requested into a newly created data
+	 * stream, that can be interogated later. 
+	 */
+	public BitInputStream getStreamBlock(int numBytes) throws java.io.IOException {
+		byte[] data = new byte[numBytes];
+		
+		getArray(data);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		DataInputStream dis = new DataInputStream(bais);
+		return new BitInputStream(dis);
+	}
+}
+
---
+++ commons-io/src/com/ironiacorp/io/BitOutputStream.java
@@ -0,0 +1,61 @@
+package com.ironiacorp.io;
+
+import java.io.OutputStream;
+
+/**
+ * Version 1.0
+ * Released under the GNU GPL
+ * @author steev
+ *
+ */
+public class BitOutputStream {
+
+	OutputStream thisOutStream;
+	short currentByte;
+	int bitReady;	// which bit do we write into next. 8 for MSB, 1 for LSB, 0 for load next
+	
+	private BitOutputStream() {}
+	
+	public BitOutputStream(OutputStream os) {
+		thisOutStream = os;
+		bitReady = 8;
+		currentByte = 0;
+	}
+
+	public void putBits(long value, int numBits) throws java.io.IOException {
+		// We need the 'numBits' LSB from value
+		while(numBits > 0) {
+			if (numBits > bitReady) {
+				// Add the first bitReady bits, then flush
+				
+				// Move the msg bits down into the LSB positions
+				// so 10000 is added as 10, for example.
+				currentByte |= value >> (numBits-bitReady);
+				
+				numBits -= bitReady;
+				flush();
+			} else {
+				// enough space for the whole thing 
+				long v = value & ((1<<numBits)-1);
+				
+				currentByte |= v << (bitReady-numBits);
+				bitReady -= numBits;
+				numBits = 0;
+			}
+		}
+	}
+	
+	public boolean isFlushReady() {
+		return bitReady == 8 ? true : false;
+	}
+	
+	public void flushToByte() throws java.io.IOException {
+		flush();
+	}
+	
+	protected void flush() throws java.io.IOException {
+		thisOutStream.write(currentByte);
+		bitReady = 8;
+		currentByte = 0;
+	}
+}
